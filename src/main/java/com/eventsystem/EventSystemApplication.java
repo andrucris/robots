d171faf4e3010ddad7b65c21ad29afcfd6030f7a
Marcela Nita <andrucris@yahoo.com>
2021-05-09 15:10:20 +0300

    added host

--- src/main/java/com/eventsystem/EventSystemApplication.java
+++ src/main/java/com/eventsystem/EventSystemApplication.java
@@ -19,7 +19,7 @@ public class EventSystemApplication {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/").allowedOrigins("http://localhost:8080");
+                registry.addMapping("/").allowedOrigins("http://52.5.119.46:80");
             }
         };
     }
